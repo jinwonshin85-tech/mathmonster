459170aa5d5a960cd5d3b889f3f72300ab5c4f8c
jinwonshin85-tech <jinwonshin85@gmail.com>
2026-09-12 22:16:32 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+var CACHE_NAME = 'monster-gugudan-v1';
+var FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-192-maskable.png',
+  './icons/icon-512-maskable.png'
+];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(key){ return key !== CACHE_NAME; })
+            .map(function(key){ return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      return cached || fetch(event.request).catch(function(){
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
